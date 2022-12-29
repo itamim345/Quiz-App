fa4da007f75403b92ca11fbfec5fe396bab27ed4
AF Tamim <itamim345@gmail.com>
2022-12-29 20:30:25 +0530

    Added two input fields using material ui

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,25 @@
+import { TextField } from '@material-ui/core';
 import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="home">
+      <div className="quiz-configure-title">
+        <h3>Configure the Quiz Options</h3>
+      </div>
+      <div className="quiz-configure-options">
+        <TextField
+          label="Enter your name"
+          variant="outlined"
+          size="small"
+        />
+        <TextField
+          select
+          label="Select Catagory"
+          variant="outlined"
+          size="small"
+        ></TextField>
+      </div>
     </div>
   );
 }
