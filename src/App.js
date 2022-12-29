527f98d4bdc4747b5cc4fc4b56475dedbc13dc16
AF Tamim <itamim345@gmail.com>
2022-12-29 19:00:56 +0530

    Added Route for home,quizes & score

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Quizes from './Pages/Quizes/Quizes';
+import Score from './Pages/Score/Score';
 
 function App() {
   return (
@@ -8,7 +11,9 @@ function App() {
       <div className="App">
         <Header/>
         <Routes>
-          {/* <Route path='/' element={} /> */}
+          <Route path='/' element={<Home/>} />
+          <Route path='/quizes' element={<Quizes/>} />
+          <Route path='/score' element={<Score/>} />
         </Routes>
       </div>
     </BrowserRouter>
