c0f47f843bdf5f29e69f28d2089bb64c16926e97
AF Tamim <itamim345@gmail.com>
2022-12-29 23:29:27 +0530

    Added useState and taken the value from input field

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,30 @@
 import { MenuItem, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import "../../Data/Categories"
 import Categories from '../../Data/Categories';
 
-export default function Home() {
+export default function Home({name, setName}) {
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
   return (
     <div className="home">
       <div className="quiz-configure-title">
         <h3>Configure the Quiz Options</h3>
       </div>
       <div className="quiz-configure-options">
-        <TextField label="Enter your name" variant="outlined" size="small" />
+        <TextField
+          label="Enter your name"
+          variant="outlined"
+          size="small"
+          onChange={(e) => setName(e.target.value)}
+        />
         <TextField
           select
           label="Select Catagory"
           variant="outlined"
           size="small"
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
         >
           {Categories.map((category) => (
             <MenuItem key={category.category} value={category.value}>
@@ -29,16 +38,18 @@ export default function Home() {
           label="Select Difficulty"
           variant="outlined"
           size="small"
+          onChange={(e) => setDifficulty(e.target.value)}
+          value={difficulty}
         >
-         <MenuItem key="Easy" value="easy">
-         Easy
-         </MenuItem>
-         <MenuItem key="Medium" value="medium">
-         Medium
-         </MenuItem>
-         <MenuItem key="Hard" value="hard">
-         Hard
-         </MenuItem>
+          <MenuItem key="Easy" value="easy">
+            Easy
+          </MenuItem>
+          <MenuItem key="Medium" value="medium">
+            Medium
+          </MenuItem>
+          <MenuItem key="Hard" value="hard">
+            Hard
+          </MenuItem>
         </TextField>
       </div>
     </div>
