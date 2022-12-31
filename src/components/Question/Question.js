de79bdccbdf884bbd0e904975fbc4e018ead4e8b
AF Tamim <itamim345@gmail.com>
2023-01-01 00:31:11 +0530

    Got the question option as button

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import ErrorMsg from '../ErrorMsg/ErrorMsg';
 
 export default function Question({ defaultQstn, setDefaultQstn, questions, options, correct_answer, score, setScore, getQuizes}) {
   const [selected, setSelected] = useState();
   const [error, setError] = useState(false);
-  return <div></div>;
+  return <div>
+    <h2>Question: {defaultQstn+1}</h2>
+    <div className='question-container'>
+      <h2>{questions[defaultQstn].question}</h2>
+      <div className="question-options">
+        {error && <ErrorMsg/>}
+        {
+          options && options.map(optn => (
+            <button>{optn}</button>
+          ))
+        }
+      </div>
+    </div>
+  </div>;
 }
