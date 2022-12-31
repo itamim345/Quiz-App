3aff609fa414e8ed72fecc21534c2be4a016fac1
AF Tamim <itamim345@gmail.com>
2022-12-31 23:35:30 +0530

    Recieved all the props

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 
-export default function Question() {
+export default function Question({ defaultQstn, setDefaultQstn, questions, options, correct_answer, score, setScore, getQuizes}) {
   const [selected, setSelected] = useState();
   const [error, setError] = useState(false);
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 }
