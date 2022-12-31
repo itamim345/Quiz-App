a3d87dcb21c09df8e34744ceb294dd4b7d513031
AF Tamim <itamim345@gmail.com>
2022-12-31 23:33:16 +0530

    created two state for selected item and error

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Question() {
+  const [selected, setSelected] = useState();
+  const [error, setError] = useState(false);
   return (
     <div>
       
