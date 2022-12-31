ad93f878ab3ba4dbb1b4be5b81528692afd828a6
AF Tamim <itamim345@gmail.com>
2022-12-31 20:22:08 +0530

    Added handle shuffle function to shuffle the answer in random order

--- src/Pages/Quizes/Quizes.js
+++ src/Pages/Quizes/Quizes.js
@@ -1,9 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Quizes({name, questions, score, setScore}) {
+    const [options, setOptions] = useState();
+    const [defaultQstn, setDefaultQstn] = useState(0);
     useEffect(() => {
-      console.log(questions);
+        setOptions(
+          questions &&
+            handleShuffle([
+              questions[defaultQstn]?.correct_answer,
+              ...questions[defaultQstn]?.incorrect_answer,
+            ])
+        );
     }, [questions]);
+
+   const  handleShuffle  = (myoptions) => {
+        return myoptions.sort(() => Math.random() - 0.5)
+    }
   return (
     <div>
       Quizes
