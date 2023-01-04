8908a2367ab4de02829eb36e006adb6922051e71
AF Tamim <itamim345@gmail.com>
2023-01-05 02:49:38 +0530

    Structural changes in home component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -23,54 +23,59 @@ export default function Home({name, setName, getQuestions}) {
     }
   return (
     <div className="home">
-      <div className="quiz-configure-title">
-        <h3>Configure the Quiz Options</h3>
+      <div className="left-home">
+        
       </div>
-      <div className="quiz-configure-options">
-        {error && <ErrorMsg/>}
-        <TextField
-          label="Enter your name"
-          variant="outlined"
-          size="small"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          select
-          label="Select Catagory"
-          variant="outlined"
-          size="small"
-          onChange={(e) => setCategory(e.target.value)}
-          value={category}
-        >
-          {Categories.map((category) => (
-            <MenuItem key={category.category} value={category.value}>
-              {category.category}
+      <div className="right-home">
+        <div className="quiz-configure-title">
+          <h3>Configure the Quiz Options</h3>
+        </div>
+        <div className="quiz-configure-options">
+          {error && <ErrorMsg />}
+          <TextField
+            label="Enter your name"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            select
+            label="Select Catagory"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            {Categories.map((category) => (
+              <MenuItem key={category.category} value={category.value}>
+                {category.category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            label="Select Difficulty"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setDifficulty(e.target.value)}
+            value={difficulty}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
             </MenuItem>
-          ))}
-        </TextField>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
 
-        <TextField
-          select
-          label="Select Difficulty"
-          variant="outlined"
-          size="small"
-          onChange={(e) => setDifficulty(e.target.value)}
-          value={difficulty}
-        >
-          <MenuItem key="Easy" value="easy">
-            Easy
-          </MenuItem>
-          <MenuItem key="Medium" value="medium">
-            Medium
-          </MenuItem>
-          <MenuItem key="Hard" value="hard">
-            Hard
-          </MenuItem>
-        </TextField>
-
-        <Button variant="contained" color="secondary" onClick={handleSubmit}>
-          Secondary
-        </Button>
+          <Button variant="contained" color="secondary" onClick={handleSubmit}>
+            Secondary
+          </Button>
+        </div>
       </div>
     </div>
   );
