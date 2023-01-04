96a0d967b065e5e1c02623c2dfb79ea7117dcc31
AF Tamim <itamim345@gmail.com>
2023-01-05 04:22:02 +0530

    Added image with style

--- src/Pages/Score/Score.js
+++ src/Pages/Score/Score.js
@@ -4,14 +4,17 @@ import "./Score.css"
 
 export default function Score({name, score}) {
   return (
-    <div className='score-container'>
+    <div className="score-container">
+      <img src="https://i.ibb.co/j8bcJnN/smily-face.png" alt="smily-face" />
       <h3>
         Congratulations! <i>{name}</i>
       </h3>
       <h2>
         Your Score is - <b>{score}</b>
       </h2>
-      <Button variant="contained" color="secondary" href='/'>Start Quiz Again</Button>
+      <Button variant="contained" color="secondary" href="/">
+        Start Quiz Again
+      </Button>
     </div>
   );
 }
